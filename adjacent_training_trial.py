0e5dd2345a323740d468fa144e71bd4670d42c7b
Ruoy <ruoyzhang@gmail.com>
2018-10-29 15:15:17 +0100

    adjusted training scheme

--- adjacent_training_trial.py
+++ adjacent_training_trial.py
@@ -11,20 +11,19 @@ data_dir_0 = '/home/paperspace/projects/news_exploration/Data/training_data/peri
 save_dir = data_dir_1
 e_dim = 300
 n_negs = 40
-epoch = 10
+epoch = 5
 mb = 1024
 ss_t = 1e-5
 conti = False
 weights = True
 cuda = True
 
-# train(name = name, data_dir_1 = data_dir_0, save_dir = data_dir_0,
-#       e_dim = e_dim, n_negs = n_negs, epoch = epoch, mb = mb,
-#       ss_t = ss_t, conti = conti, weights = weights, cuda = cuda, data_dir_0 = None)
+train(name = name, data_dir_1 = data_dir_0, save_dir = data_dir_0,
+      e_dim = e_dim, n_negs = n_negs, epoch = epoch, mb = mb,
+      ss_t = ss_t, conti = conti, weights = weights, cuda = cuda, data_dir_0 = None)
 
-# print('first period training terminated')
+print('first period training terminated')
 
-epoch = 5
 
 train(name = name, data_dir_1 = data_dir_1, save_dir = save_dir,
       e_dim = e_dim, n_negs = n_negs, epoch = epoch, mb = mb,
