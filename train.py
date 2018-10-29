977cfac288f118b6d0df340ae51893ad4c5342d4
Ruoy <ruoyzhang@gmail.com>
2018-10-29 12:42:17 +0100

    fixed order issue

--- train.py
+++ train.py
@@ -97,9 +97,9 @@ def train(name, data_dir_1, save_dir, e_dim, n_negs, epoch, mb, ss_t, conti, wei
             # if cuda:
             #     iword = iword.cuda()
             #     owords = owords.cuda()
-            vocab_present = list(set(iword.cpu().numpy())&set(idx2idx.keys()))
             if data_dir_0 is not None and len(vocab_present) != 0:
                 # here we need to create a idx2idx dict
+                vocab_present = list(set(iword.cpu().numpy())&set(idx2idx.keys()))
                 rwords_dict = {word:idx2idx[word] for word in vocab_present}
             else:
                 rwords_dict = None
