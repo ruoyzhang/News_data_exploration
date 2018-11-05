be61b923be5621c2cbb1d9a8741916732d55401a
Ruoy <ruoyzhang@gmail.com>
2018-11-05 10:51:12 +0100

    correcting indentation issues

--- preprocessing.py
+++ preprocessing.py
@@ -85,9 +85,9 @@ class news_preprocess:
 			begin_dates.append(begin_date)
 			begin_date += datetime.timedelta(days = period)
 			with open(data_dir + 'articles_preprocessed_' + str(i) + '.pickle', 'wb') as handle:
-	 			pickle.dump(articles, handle, protocol = pickle.HIGHEST_PROTOCOL)
-	 		print('there are ', len(articles), 'articles in period '  str(i))
-	 		print('preprocessing finished for period '+str(i) + ', it has been saved')
+				pickle.dump(articles, handle, protocol = pickle.HIGHEST_PROTOCOL)
+			print('there are ', len(articles), 'articles in period '  str(i))
+			print('preprocessing finished for period ' + str(i) + ', it has been saved')
 		self.begin_dates = begin_dates
 		with open(data_dir + 'articles_begin_dates.pickle', 'wb') as handle:
 			pickle.dump(self.begin_dates, handle, protocol = pickle.HIGHEST_PROTOCOL)
